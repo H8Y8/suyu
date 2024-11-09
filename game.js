511fcc3ac5f585684ccf7c11fc4100f89b1607e2
H8Y8 <110684975+H8Y8@users.noreply.github.com>
2024-11-09 12:35:28 +0800

    Update game.js

--- game.js
+++ game.js
@@ -25,7 +25,7 @@ class Game {
         this.difficulty = { value: 'hard' };
         this.difficultySettings = {
             hard: { 
-                moveInterval: 500,
+                moveInterval: 350,
                 points: 15 
             }
         };
